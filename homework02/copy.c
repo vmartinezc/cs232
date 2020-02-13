208ddafe7768fde7aaeefcadf1dfbc8021c7b626
vmartinezc <vamc98@hotmail.com>
2020-02-13 18:45:28 -0500

    Some error conditions handled

--- homework02/copy.c
+++ homework02/copy.c
@@ -14,11 +14,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (){
+int main (int argc, char *argv[]){
+//argc takes in the numner of arguments passed && argv[] is a pointer array for each argument 
+
+    if(argc < 2){
+        perror("Missing parameter. Try: copy <srcpath> <destinationpath>");
+        exit(-1);
+    }
+    else if (argv[2] == NULL) {
+        perror("Please provide source path and destination path");
+        exit(-1);
+    }
+
+    //TO-DO:
+    //check if file exists
+
+    else{
+        printf("The source path was: %s\n", argv[1]);
+        printf("The destination path was: %s\n", argv[2]);
+    }
+
+
+
+
+
 
     
-
-
 
 
 
