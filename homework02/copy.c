fe0a57e08072e9dc7b6f582f8f99e852e3963dc5
vmartinezc <vamc98@hotmail.com>
2020-02-13 17:38:49 -0500

    Initial commit bois

---
+++ homework02/copy.c
@@ -0,0 +1,25 @@
+/**  Homework 02
+ 
+* *  Writing a Program in C
+* *  @author: Valeria Martinez (vam6)
+* *  @date: 02-13-2020 
+
+*/ 
+
+
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+    
+
+
+
+
+
+}
