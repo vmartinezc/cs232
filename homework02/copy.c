4933bbacf0f8597d56c12bcf0cacf4d58820a338
vmartinezc <vamc98@hotmail.com>
2020-02-15 11:14:20 -0500

    doesFileExist function

--- homework02/copy.c
+++ homework02/copy.c
@@ -11,6 +11,25 @@
 #include <unistd.h>
 
 
+/* * doesFileExist
+* *  @param: filename
+* * return 0 if file exists
+* * -1 is it does not exist
+* */ 
+
+int doesFileExist(const char *file){
+    if (access(file,F_OK)==0){
+        return 0;
+    }
+    else{
+        return -1;
+    }
+}
+
+
+
+
+
 int main (int argc, char *argv[]){
     //argc takes in the numner of arguments passed && argv[] is a pointer array for each argument 
 
@@ -26,6 +45,11 @@ int main (int argc, char *argv[]){
     else if (argv[2] == NULL) {
         perror("Please provide source path and destination path");
         exit(-1);
+    }
+
+    else if( access(const char *srcDest, F_OK) = 0){
+        perror("File already exists");
+
     }
 
     //TO-DO:
