da1efb3ee2335a7b0f126001d53663f6be077857
vmartinezc <vamc98@hotmail.com>
2020-02-17 16:03:16 -0500

    changes changes

--- homework02/copy.c
+++ homework02/copy.c
@@ -11,18 +11,11 @@
 #include <unistd.h>
 
 
-
-//TO-DO:
-//check if file exists
-//use access function if it equals 0, then it means that file dest already exists
-
-
-/* * doesFileExist
-* *  @param: filename
-* * return 0 if file exists
-* * -1 is it does not exist
-* */ 
-
+/*  doesFileExist
+*   @param: filename
+*  return 0 if file exists
+*  -1 is it does not exist
+*/ 
 int doesFileExist(const char *file){
     if (access(file,F_OK)==0){ 
         return 0;
@@ -32,39 +25,28 @@ int doesFileExist(const char *file){
     }
 }
 
-
-/* * checkArgs
-* * @argc: int, takes in number of args
-* * @argv  pointer array, store each argument
-* * return -1 if error, 0 if all args are valid
-* */ 
-
+/*  checkArgs
+*   @argc: int, takes in number of args
+*   @argv  pointer array, stores each argument
+*   return -1 if error, 0 if all args are valid
+*/
 int checkArgs(int argc, char *argv[]){
     if (argc<2){
         perror("Missing parameter. \nTry: copy <srcpath><destination path>");
         exit(-1);
     }
-
     else if (argv[2] == NULL){
         perror("Please provide source path and destination path.");
         exit(-1);
     }
-
     else {
         printf("The source path was: %s\n", argv[1]);
         printf("The destination path was: %s\n", argv[2]);
         return 0;
-
     }
 }
 
-
-
-
-
-
-int main (int argc, char *argv[]){
-    //argc takes in the numner of arguments passed && argv[] is a pointer array for each argument 
+int main (int argc, char *argv[]){  
 
     const char *srcPath;
     const char *srcDest;
