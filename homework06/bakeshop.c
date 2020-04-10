86335a28ae6d0965c0231ba38a6bc40d673d4a92
vmartinezc <vamc98@hotmail.com>
2020-04-10 16:49:00 -0400

    Bakeshop initial code

---
+++ homework06/bakeshop.c
@@ -0,0 +1,14 @@
+/*
+    Busy Bakeshop: 
+    This assignment explores concurrency and semaphores.
+    The program runs several different threads. 
+    The concurrent threads use semaphores to coordinate the resources in a simple baker
+
+
+    @author: Valeria Martínez Castro (vam6)
+    @date: 11 April 2020
+    CS232 @ Calvin University
+
+ */
+
+
