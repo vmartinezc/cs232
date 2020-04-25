82c5e737af2292ea3777e834e4348f8483fc49ad
vmartinezc <vamc98@hotmail.com>
2020-04-25 17:19:55 -0400

    homework 7

---
+++ homework07/homework07/ram.py
@@ -0,0 +1,33 @@
+RAM_SIZE = 1024
+
+
+class RAM:
+    def __init__(self, size=RAM_SIZE):
+        self._minAddr = 0
+        self._maxAddr = RAM_SIZE - 1
+        self._memory = []   # a list of values.  Could be #s or instructions.
+        for i in range(size):
+            self._memory.append(0)
+
+    def __getitem__(self, addr):
+        return self._memory[addr]
+
+    def __setitem__(self, addr, val):
+        self._memory[addr] = val
+
+    def is_legal_addr(self, addr):
+        return self._minAddr <= addr <= self._maxAddr
+
+class MMU:
+    def __init__(self, ram):
+        self.ram = ram
+        
+    def get_ram_value(self, addr):
+        return self.ram.__getitem__(addr)
+    
+    def set_ram_value(self, addr, value):
+        return self.ram.__setitem__(addr,value)
+    
+    
+    
+        
