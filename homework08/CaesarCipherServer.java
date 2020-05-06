1a5d7650e4c243b8dc744c71337c6f9e88f89e0a
vmartinezc <vamc98@hotmail.com>
2020-05-05 21:54:06 -0400

    caesar cipher

---
+++ homework08/CaesarCipherServer.java
@@ -0,0 +1,22 @@
+/* Homework 8: Caesar Cipher Client
+ * Valeria Martinez (vam6)
+ * May 9, 2020
+ *
+ * Client gets the server's hostname and port via command-line arguments.
+ */
+
+
+
+import java.util.Scanner;
+import java.net.Socket;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class CaesarCipherClient{
+    public static void main(String[] args){
+        System.out.print("Hello how r u");
+    }
+}
